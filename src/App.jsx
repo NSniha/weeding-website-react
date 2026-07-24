34228050a855ad8c5560d76449406e79e3c3aa02
Nobonita Saha Niha <nobonitasaha042@gmail.com>
2026-07-24 23:50:44 +0600

    Updated app code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
 
       <main>
         <Hero />
+        <About />
       </main>
     </>
   );
