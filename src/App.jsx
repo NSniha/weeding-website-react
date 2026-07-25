e86f4438ed0c20ce53c20e1ba48e697e07ab62ba
Nobonita Saha Niha <nobonitasaha042@gmail.com>
2026-07-25 23:07:14 +0600

    Styling feature section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
 import CreativeStory from "./components/CreativeStory/CreativeStory";
+import FeaturedStory from "./components/FeaturedStory/FeaturedStory";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <About />
         <Services/>
         <CreativeStory/>
+        <FeaturedStory/>
       </main>
     </>
   );
